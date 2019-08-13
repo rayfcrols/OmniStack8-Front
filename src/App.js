68e6de47c68b259c8f4ccc7881abb28f8aaabcbf
Raimundo Luiz <rayfcrols@gmail.com>
2019-08-13 17:16:05 -0300

    Finalizado o Frontend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
 
-import Login from "./pages/Login";
+import Routes from "./routes";
 
 export default function App() {
-  return <Login />;
+  return <Routes />;
 }
